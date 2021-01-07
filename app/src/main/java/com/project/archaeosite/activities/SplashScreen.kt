8ebf087a1b903adcd14246f823d30f61a9618c15
gohpeijin <gohpeijin@gmail.com>
2021-01-07 19:08:28 +0800

    Splash screen (updated)

--- app/src/main/java/com/project/archaeosite/activities/SplashScreen.kt
+++ app/src/main/java/com/project/archaeosite/activities/SplashScreen.kt
@@ -1,15 +1,25 @@
 package com.project.archaeosite.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import com.project.archaeosite.R
 
 class SplashScreen : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
 
-        println();
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this,DisplayListsActivity::class.java))
+            finish()
+        }, 3000)
+
+
     }
 
 
