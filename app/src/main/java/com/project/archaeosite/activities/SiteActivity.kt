87861717c39c9353eddc48f14c4e12e44e4fe830
gohpeijin <gohpeijin@gmail.com>
2021-01-08 00:07:57 +0800

    can select multiple image & display multiple image by clicking next&previous button (still cant change image, it will add on the image)

--- app/src/main/java/com/project/archaeosite/activities/SiteActivity.kt
+++ app/src/main/java/com/project/archaeosite/activities/SiteActivity.kt
@@ -63,8 +63,28 @@ class SiteActivity : AppCompatActivity(), AnkoLogger {
             showImagePicker(this,IMAGE_REQUEST)
         }
 
+        var imageposition=0
         button_next_image.setOnClickListener(){
-
+            if(imageposition<site.image.size-1)
+            {
+                imageposition++
+                ImageSelected.setImageBitmap(readImageFromPath(this, site.image!!.get(imageposition)))
+            }
+            else
+            {
+                toast("No more images")
+            }
+        }
+        button_previos_image.setOnClickListener(){
+            if(imageposition>0)
+            {
+                imageposition--
+                ImageSelected.setImageBitmap(readImageFromPath(this, site.image!!.get(imageposition)))
+            }
+            else
+            {
+                toast("Reach the first image")
+            }
         }
         item_back.setOnClickListener(){
             info("back icon pressed")
@@ -76,43 +96,6 @@ class SiteActivity : AppCompatActivity(), AnkoLogger {
         }
     }
 
-//    lateinit var imagePath: String
-//    fun getPathFromURI(uri: Uri) {
-//        var path: String = "uri.path"// uri = any content Uri
-//        val databaseUri: Uri
-//        val selection: String?
-//        val selectionArgs: Array<String>?
-//        if (path.contains("/document/image:")) { // files selected from "Documents"
-//            databaseUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
-//            selection = "_id=?"
-//            selectionArgs = arrayOf(DocumentsContract.getDocumentId(uri).split(":")[1])
-//        } else { // files selected from all other sources, especially on Samsung devices
-//            databaseUri = uri
-//            selection = null
-//            selectionArgs = null
-//        }
-//        try {
-//            val projection = arrayOf(
-//                MediaStore.Images.Media.DATA,
-//                MediaStore.Images.Media._ID,
-//                MediaStore.Images.Media.ORIENTATION,
-//                MediaStore.Images.Media.DATE_TAKEN
-//            ) // some example data you can query
-//            val cursor = contentResolver.query(
-//                databaseUri,
-//                projection, selection, selectionArgs, null
-//            )
-//            if (cursor!!.moveToFirst()) {
-//                val columnIndex = cursor.getColumnIndex(projection[0])
-//                imagePath = cursor.getString(columnIndex)
-//                // Log.e("path", imagePath);
-//                site.image.add(imagePath)
-//            }
-//            cursor.close()
-//        } catch (e: Exception) {
-//           // Log.e(TAG, e.message, e)
-//        }
-//    }
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         when (requestCode) {
@@ -120,31 +103,17 @@ class SiteActivity : AppCompatActivity(), AnkoLogger {
                 if(data!!.clipData!=null){
                     val count=data.clipData!!.itemCount
                     for(i in 0 until count){
-//                        site.image!!.add(data.getData().toString())
                       val imageUri=data.clipData!!.getItemAt(i).uri
-//                        site.image!!.add(imageUri)
-//                        getPathFromURI(imageUri)
                         site.image!!.add(imageUri.toString())
-
                     }
                         ImageSelected.setImageBitmap(readImageFromPath(this, site.image!!.get(0)))
                 }
                 else{
-                    //val iamgeUri=data.data
                     site.image!!.add(data.getData().toString())
                     ImageSelected.setImageBitmap(readImage(this, resultCode, data))
 
-                    //readImage(this, resultCode, data)
                 }
-                //content://com.android.providers.media.documents/document/image%3A229292
-               // ImageSelected.setImageBitmap(site.image.get(0))
-//                if (data != null) {
-//                    info("setting up image")
-//                   // site.image = data.getData().toString()
-//                    site.image!!.add(data.getData().toString())
-//                 //  site.image ="content://com.android.externalstorage.documents/document/3232-6531%3ADalat%2F20191005_130946_018_saved.jpg"
-//                    ImageSelected.setImageBitmap(readImage(this, resultCode, data))
-//                }
+
             }
         }
     }
