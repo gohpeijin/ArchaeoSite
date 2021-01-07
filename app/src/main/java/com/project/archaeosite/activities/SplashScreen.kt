4385b6577298f90bfbb7e0a62a31cb5fe359d7f6
gohpeijin <gohpeijin@gmail.com>
2021-01-07 16:49:54 +0800

    Splash screen

---
+++ app/src/main/java/com/project/archaeosite/activities/SplashScreen.kt
@@ -0,0 +1,16 @@
+package com.project.archaeosite.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.project.archaeosite.R
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        println();
+    }
+
+
+}
