9239d546ffbcf56d131379b6c745689b286691ec
gohpeijin <gohpeijin@gmail.com>
2021-01-09 03:39:58 +0800

    add baseview,basepresenter

---
+++ app/src/main/java/com/project/archaeosite/view/base/BasePresenter.kt
@@ -0,0 +1,20 @@
+package com.project.archaeosite.view.base
+
+import android.content.Intent
+import com.project.archaeosite.main.MainApp
+
+open class BasePresenter (var view:BaseView?){
+
+    var app: MainApp =  view?.application as MainApp
+
+    open fun doActivityResult(requestCode: Int, resultCode: Int, data: Intent) {}
+
+    open fun doRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+    }
+
+    open fun onDestroy() {
+        view = null
+    }
+
+
+}
